45a3a33607a81e3e79efbdac3b94503a62f1134d
Leif Lundberg <leif.a.lundberg@tietoevry.com>
2021-11-20 16:18:35 +0100

    Cleanup

--- Intro-Tree/BinaryTree.java
+++ Intro-Tree/BinaryTree.java
@@ -27,14 +27,3 @@ class BinaryTree {
     */
     }
 }
-
-class Node {
-    int key;
-    Node left, right; 
-
-    Node(int i) {
-        key = i;
-        left = null; 
-        right = null;
-    }
-}
